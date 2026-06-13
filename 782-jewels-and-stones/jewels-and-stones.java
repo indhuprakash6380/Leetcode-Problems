316f0340c3cc5e95c93502c100491f25fafb47cb
INDHUPRAKASH S D <indhuprakash6380@gmail.com>
2026-06-13 12:54:14 +0530

    Time: 1 ms (59.83%) | Memory: 43.2 MB (51.40%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        // char c[]=s1.toCharArray();
+        int count=0;
+        for(int i=0;i<stones.length();i++)
+        {
+            char c=stones.charAt(i);
+            if(jewels.indexOf(c)!=-1)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
